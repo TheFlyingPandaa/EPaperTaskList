0f660c43a2c39a7a397d2fe16f046b785f69b93a
TheFlyingPandaa <joakim.trossvik@gmail.com>
2020-09-06 19:39:52 +0200

    Printing stuff from the text

--- parseTaskyOutput.py
+++ parseTaskyOutput.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse
+import epd2in7
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 def main():
 
@@ -56,6 +60,24 @@ def main():
 
     taskyRawOutput.close()
 
+    epd = epd2in7.EPD()
+    epd.init()
+
+    # For simplicity, the arguments are explicit numerical coordinates
+    finalImage = Image.new('1', (epd2in7.EPD_WIDTH, epd2in7.EPD_HEIGHT), 255)    # 255: clear the image with white
+    image = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 0)    # 255: clear the image with white
+
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 18)
+    counter = 0
+    for inp in finalList:
+        draw.text((20, ((counter)*20)), inp[2], font = font, fill = 255)
+        counter += 1
+
+    image = image.rotate(90, expand=1)
+    finalImage.paste(image, (0,0))
+
+    epd.display_frame(epd.get_frame_buffer(finalImage))
 
 if __name__ == '__main__':
     main()
